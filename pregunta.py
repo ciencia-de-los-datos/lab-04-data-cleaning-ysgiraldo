bf4ae855e479123baa3373cc927de673abed0c0a
ysgiraldo <ygiraldoca@unal.edu.co>
2024-03-19 22:36:54 -0500

    Lab 04 resuelto

--- pregunta.py
+++ pregunta.py
@@ -11,10 +11,44 @@ import pandas as pd
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
     #
     # Inserte su código aquí
     #
+    
+    # Delete de NA values
+    df.dropna(inplace=True)
 
+    # All the str lower
+    df = df.apply(lambda x: x.str.lower() if x.dtype == "object" else x)
+    # Delete regular expression that we do not need
+    df = df.apply(lambda x: x.str.replace('_', '-') if x.dtype == "object" else x)
+    df = df.apply(lambda x: x.str.replace('-', ' ') if x.dtype == "object" else x)
+
+    # The format will be yyyy-mm-dd but if the date is already 
+    # in the desired format, the function will not modify it
+    def convert_date(date):
+        try:
+            return pd.to_datetime(date, format='%d/%m/%Y')
+        except ValueError:
+            return pd.to_datetime(date, errors='coerce')
+    df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(convert_date)
+
+    # This will leave the format as float after removing the respective strings 
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',', '').str.replace('$', '').str.extract('(\d+)').astype(float)
+    
+    # Delete duplicates
+    df.drop_duplicates(inplace=True)
+    
     return df
+
+# def main():
+#     clean_df = clean_data()
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+# print(clean_data())
